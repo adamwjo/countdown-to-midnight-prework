1357d5ffe1aa29cd38bfab06657dd4ec1beb6df9
Adam Johnson <adamwjo@gmail.com>
2018-07-21 15:10:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,10 +4,10 @@
 
 def countdown(number)
   while number > 0
-    puts "#{number} SECOND(S)!"
     def countdown_with_sleep(num)
       sleep(num)
     end
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
   "HAPPY NEW YEAR!"
